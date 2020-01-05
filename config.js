8d4a2c07b450c9b213c5cedeb4d88df1a985816d
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-04 22:29:14 -0300

    some fix

--- config.js
+++ config.js
@@ -1,46 +1,46 @@
-"use strict";
-
-module.exports = {
-  url: "https://malaquias.dev",
-  pathPrefix: "/",
-  title: "MalaquiasDEV | A Vida, o código e tudo mais",
-  subtitle: "",
+const config = {
+  url: 'https://malaquias.dev',
+  pathPrefix: '/',
+  title: 'MalaquiasDEV | A Vida, o código e tudo mais',
+  subtitle: '',
   copyright:
-    "Conteúdo feito com 💞 por Mateus Malaquias. Site desenvolvido por alxshelepenok e malaquiasdev. Este trabalho está licenciado com uma Licença Internacional Creative Commons Attribution-NonCommercial 4.0.",
-  disqusShortname: "",
+    'Conteúdo feito com 💞 por Mateus Malaquias. Site desenvolvido por alxshelepenok e malaquiasdev. Este trabalho está licenciado com uma Licença Internacional Creative Commons Attribution-NonCommercial 4.0.',
+  disqusShortname: '',
   postsPerPage: 5,
-  googleAnalyticsId: "UA-68403924-1",
+  googleAnalyticsId: 'UA-68403924-1',
   useKatex: false,
   menu: [
     {
-      label: "Artigos",
-      path: "/"
+      label: 'Artigos',
+      path: '/'
     },
     {
-      label: "Palestras",
-      path: "/pages/palestras"
+      label: 'Palestras',
+      path: '/pages/palestras'
     }
   ],
   author: {
-    name: "Mateus Malaquias",
-    photo: "/photo.jpg",
+    name: 'Mateus Malaquias',
+    photo: '/photo.jpg',
     bio:
-      "Baiano | Engenheiro de software na Claro Brasil | Gosto de compartilhar conhecimento escrevendo sobre coisas de tech ☕️",
+      'Baiano | Engenheiro de software na Claro Brasil | Gosto de compartilhar conhecimento escrevendo sobre coisas de tech ☕️',
     contacts: {
-      email: "mateusmalaquiasdev@outlook.com",
-      facebook: "mmalaquiasdev",
-      telegram: "malaquiasdev",
-      twitter: "malaquiasdev",
-      github: "malaquiasdev",
-      rss: "",
-      vkontakte: "",
-      linkedin: "mateus-malaquias-692532b5",
-      instagram: "malaquiasdev",
-      line: "",
-      gitlab: "",
-      weibo: "",
-      codepen: "",
-      youtube: ""
+      email: 'mateusmalaquiasdev@outlook.com',
+      facebook: 'mmalaquiasdev',
+      telegram: 'malaquiasdev',
+      twitter: 'malaquiasdev',
+      github: 'malaquiasdev',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'mateus-malaquias-692532b5',
+      instagram: 'malaquiasdev',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: ''
     }
   }
 };
+
+module.exports = config;
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const siteConfig = require("./config.js");
-const postCssPlugins = require("./postcss-config.js");
-require("dotenv").config();
+const siteConfig = require('./config.js');
+const postCssPlugins = require('./postcss-config.js');
+require('dotenv').config();
 
 process.env.AWS_ACCESS_KEY_ID;
 process.env.AWS_SECRET_ACCESS_KEY;
@@ -20,41 +20,41 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-s3",
+      resolve: 'gatsby-plugin-s3',
       options: {
-        bucketName: process.env.S3_BUCKET_NAME || "develop"
+        bucketName: process.env.S3_BUCKET_NAME || 'develop'
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content`,
-        name: "pages"
+        name: 'pages'
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/media`,
-        name: "media"
+        name: 'media'
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "css",
+        name: 'css',
         path: `${__dirname}/static/css`
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "assets",
+        name: 'assets',
         path: `${__dirname}/static`
       }
     },
     {
-      resolve: "gatsby-plugin-feed",
+      resolve: 'gatsby-plugin-feed',
       options: {
         query: `
           {
@@ -76,7 +76,7 @@ module.exports = {
                 date: edge.node.frontmatter.date,
                 url: site.siteMetadata.site_url + edge.node.fields.slug,
                 guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                custom_elements: [{ "content:encoded": edge.node.html }]
+                custom_elements: [{ 'content:encoded': edge.node.html }]
               })),
             query: `
               {
@@ -103,25 +103,25 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
+            output: '/rss.xml',
             title: siteConfig.title
           }
         ]
       }
     },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          "gatsby-remark-relative-images",
+          'gatsby-remark-relative-images',
           {
-            resolve: "gatsby-remark-katex",
+            resolve: 'gatsby-remark-katex',
             options: {
-              strict: "ignore"
+              strict: 'ignore'
             }
           },
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960,
               withWebp: true,
@@ -129,28 +129,28 @@ module.exports = {
             }
           },
           {
-            resolve: "gatsby-remark-responsive-iframe",
-            options: { wrapperStyle: "margin-bottom: 1.0725rem" }
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
           },
-          "gatsby-remark-autolink-headers",
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
-          "gatsby-remark-external-links"
+          'gatsby-remark-autolink-headers',
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-external-links'
         ]
       }
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-netlify",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify',
     {
-      resolve: "gatsby-plugin-netlify-cms",
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/index.js`
       }
     },
     {
-      resolve: "gatsby-plugin-google-gtag",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         trackingIds: [siteConfig.googleAnalyticsId],
         pluginConfig: {
@@ -159,7 +159,7 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-plugin-sitemap",
+      resolve: 'gatsby-plugin-sitemap',
       options: {
         query: `
           {
@@ -181,32 +181,32 @@ module.exports = {
             }
           }
         `,
-        output: "/sitemap.xml",
+        output: '/sitemap.xml',
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map(edge => ({
             url: site.siteMetadata.siteUrl + edge.node.path,
-            changefreq: "daily",
+            changefreq: 'daily',
             priority: 0.7
           }))
       }
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: siteConfig.title,
         short_name: siteConfig.title,
-        start_url: "/",
-        background_color: "#FFF",
-        theme_color: "#F7A046",
-        display: "standalone",
-        icon: "static/photo.jpg"
+        start_url: '/',
+        background_color: '#FFF',
+        theme_color: '#F7A046',
+        display: 'standalone',
+        icon: 'static/photo.jpg'
       }
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-offline',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: 'gatsby-plugin-sass',
       options: {
         postCssPlugins: [...postCssPlugins],
         cssLoaderOptions: {
@@ -214,7 +214,7 @@ module.exports = {
         }
       }
     },
-    "gatsby-plugin-flow",
-    "gatsby-plugin-optimize-svgs"
+    'gatsby-plugin-flow',
+    'gatsby-plugin-optimize-svgs'
   ]
 };
